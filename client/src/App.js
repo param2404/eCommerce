d95b5e36f09f09105b48ad194c4485a4d64ba33f
Param4895 <paramjeet.kaur@mail.vinove.com>
2020-10-15 13:45:27 +0530

    client design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Navbar from './components/Common/Navbar'
+import Footer from './components/Common/Footer'
+import Home from './components/Home/index'
 
 function App() {
-  return (
-   <h1>HI</h1>
+  return (<div className="d-flex" style={{flexDirection:'column', justifyContent:'space-between',height:'100vh'}}>
+    <Navbar />
+    <Home/>
+    <Footer />
+    </div>
   );
 }
 
---
+++ client/src/components/Common/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+    return (<div className="d-flex" style={{ backgroundColor: '#E7473A', height: '20vh', opacity: '0.95', alignItems: 'center', justifyContent: 'center' }}>
+        <p style={{ color: '#FFF' }}>Copyright @2020</p>
+    </div>
+    )
+}
+
+export default Footer
