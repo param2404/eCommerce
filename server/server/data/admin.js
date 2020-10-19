e2065796cde388af6af6d3d6501bb115e7646bdf
Param4895 <paramjeet.kaur@mail.vinove.com>
2020-10-19 12:53:13 +0530

    server

--- server/server/data/admin.js
+++ server/server/data/admin.js
@@ -2,12 +2,12 @@ const bcrypt = require('bcryptjs')
 
 const admin = [
     {
-        name: 'Paramjeet',
+        name: process.env.ADMIN_NAME,
         dob: '1995-08-04',
         age: 25,
-        contactNumber: 9501463093,
-        email: 'pkd2212870@gmail.com',
-        password: bcrypt.hashSync('imadmin', 10),
+        contactNumber: process.env.ADMIN_CONTACT,
+        email: process.env.ADMIN_EMAIL,
+        password: bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10),
         userType:1
     }
 ]
