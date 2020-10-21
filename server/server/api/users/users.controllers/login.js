a7f2e12c73436047dc2e9abdb403aa64005f2299
Param4895 <paramjeet.kaur@mail.vinove.com>
2020-10-21 16:26:33 +0530

    registerUser & login

--- server/server/api/users/users.controllers/login.js
+++ server/server/api/users/users.controllers/login.js
@@ -5,9 +5,45 @@ const _ = require("lodash");
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+//////////////////////VALIDATIONS////////////////////////////////
+const loginSchema = Joi.object().keys({
+    email: Joi.string()
+        .trim()
+        .required()
+        .lowercase()
+        .max(50)
+        .email({
+            minDomainSegments: 2,
+            tlds: {
+                allow: ["com", "net", "in"],
+            },
+        })
+        .messages({
+            "string.empty": "Email is required",
+            "string.max": "Email should be less than 50 letter",
+            "any.required": "Email is required",
+        }),
+    password: Joi.string().trim().max(20).min(8).required().messages({
+        "string.base": "password should be string",
+        "string.empty": "password required",
+        "string.max": "password should be less than 20 letter",
+        "string.min": "password should be minimum 8 letter",
+        "any.required": "password is required",
+    })
+})
+///////////////////////////////////////////////////////////
+
 
 const login = asyncHandler(async (req, res) => {
     try {
+        let result = await loginSchema.validate(req.body)
+        if (result.error) {
+            return Services._validationError(
+                res,
+                result.error.details[0].message,
+                "Validation Error"
+            );
+        }
         const user = await Users
             .findOne({ email: req.body.email }, { __v: 0, createdAt: 0, updatedAt: 0 })
         if (!user) {
